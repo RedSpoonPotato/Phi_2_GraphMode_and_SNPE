5399d2cca772f5c5633178037ec8fd2747250da0
RedSpoonPotato <96994141+RedSpoonPotato@users.noreply.github.com>
2024-07-12 15:55:26 -0700

    Add files via upload

---
+++ run.sh
@@ -0,0 +1,28 @@
+set -e
+
+# for linking with libmain.so
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/kernal1/QM_Sandbox/htp/jni/obj/local/x86_64-linux-clang
+
+#cd jni
+#make clean
+#make
+#cd ../
+#make clean
+#make
+
+# restore this
+# ./jni/load_test cpu32 2
+
+# remove this
+valgrind --tool=massif --threshold=0.1 ./jni/load_test cpu32 2
+
+######################################################################
+# OLD STUFF
+# would need to change load_test.cpp to run this
+# ./jni/load_test cpu32 ./model_generation/matmul_reshape.dlc 100
+
+# ./jni/load_test dsp16 ./model_generation/matmul.dlc 1000
+
+# python test.py
+# ./jni/load_test cpu32 ./model_generation/matmul_2.dlc 10000
+
